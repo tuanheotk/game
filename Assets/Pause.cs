f558365f5f6aefad20416eb696ff9159299080dc
tuanheotk <tuanheotk@gmail.com>
2018-05-28 22:51:18 +0700

    Update Pause in Game Tomorow Fix Return MainMenu

---
+++ Assets/Pause.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine;
+public class Pause : MonoBehaviour {
+
+	public static bool GameIsPaused = false;
+	public GameObject pauseMenuUI;
+	void Update () {
+		if( Input.GetKeyDown (KeyCode.Escape)){
+			if (GameIsPaused)
+				Resume ();
+			else
+				Pauses ();
+		}
+	}
+	void Resume(){
+		pauseMenuUI.SetActive(true);
+		Time.timeScale = 1f;
+		GameIsPaused = false;
+	}
+	void Pauses(){
+		pauseMenuUI.SetActive(true);
+		Time.timeScale = 0f;
+		GameIsPaused = true;
+	}
+
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour {
+
+	public string mainMenuScene; 
+	public GameObject pauseMenu;
+	public bool isPaused;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.P)) {
+			if (isPaused) {
+				ResumeGame ();
+			} else {
+				isPaused = true;
+				pauseMenu.SetActive (true);
+				Time.timeScale = 0f;
+			}
+
+		}
+	}
+	public void ResumeGame(){
+		isPaused = false;
+		pauseMenu.SetActive (false);
+		Time.timeScale = 1f;
+	}
+
+	public void ReturnToMain(){
+		SceneManager.LoadScene (mainMenuScene);
+	}
+}
